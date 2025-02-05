ae5f4619da74795df719fab87e2ac81f8d0cfc12
perezShaked <perez.shaked@gmail.com>
2025-02-05 15:13:02 +0200

    feat: add basic server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
-console.log('shaked');
+import express from 'express';
+import { apiRouter } from './api';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use('/', apiRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
