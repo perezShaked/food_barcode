a357cc7608863490a0ab5e37c33835dba20533e8
perezShaked <perez.shaked@gmail.com>
2025-02-08 17:34:01 +0200

    feat: add navBar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
 import { Home, AddNewCategory, AddNewProduct, ScanProduct, StockList } from './pages';
+import { Navbar } from './components';
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <h1>אפליקצייה לניהול המלאי במחסן</h1>
-      <Link to={'/'}>בית</Link>
-      <br />
-      <Link to={'/addNewCategory'}>קטגוריה חדשה</Link>
-      <br />
-      <Link to={'/addNewProduct'}>פריט חדש</Link>
-      <br />
-      <Link to={'/scanProduct'}>סרוק מוצר</Link>
-      <br />
-      <Link to={'/stockList'}>רשימת מלאי</Link>
-
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/addNewCategory" element={<AddNewCategory />}></Route>
